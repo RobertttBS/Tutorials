55b9487d4b5eb49649e2876895176f72206530db
RobertttBS <k0983357798@gmail.com>
2024-07-13 11:10:40 +0100

    Format code style of procfs tutorial.

--- Linux/Device_Driver/procfs/driver.c
+++ Linux/Device_Driver/procfs/driver.c
@@ -15,20 +15,13 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
-#include<linux/slab.h>                 //kmalloc()
-#include<linux/uaccess.h>              //copy_to/from_user()
+#include <linux/slab.h>                 //kmalloc()
+#include <linux/uaccess.h>              //copy_to/from_user()
 #include <linux/ioctl.h>
-#include<linux/proc_fs.h>
+#include <linux/proc_fs.h>
 #include <linux/err.h>
+#include <linux/version.h>
 
-/* 
-** I am using the kernel 5.10.27-v7l. So I have set this as 510.
-** If you are using the kernel 3.10, then set this as 310,
-** and for kernel 5.1, set this as 501. Because the API proc_create()
-** changed in kernel above v5.5.
-**
-*/ 
-#define LINUX_KERNEL_VERSION  510
  
 #define WR_VALUE _IOW('a','a',int32_t*)
 #define RD_VALUE _IOR('a','b',int32_t*)
@@ -76,7 +69,7 @@ static struct file_operations fops =
 };
 
 
-#if ( LINUX_KERNEL_VERSION > 505 )
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
 
 /*
 ** procfs operation sturcture
@@ -125,23 +118,18 @@ static int release_proc(struct inode *inode, struct file *file)
 */
 static ssize_t read_proc(struct file *filp, char __user *buffer, size_t length,loff_t * offset)
 {
-    pr_info("proc file read.....\n");
-    if(len)
-    {
-        len=0;
-    }
-    else
-    {
-        len=1;
-        return 0;
-    }
-    
-    if( copy_to_user(buffer,etx_array,20) )
-    {
-        pr_err("Data Send : Err!\n");
-    }
- 
-    return length;;
+        pr_info("proc file read.....\n");
+        if (len)
+                len = 0;
+        else {
+                len=1;
+                return 0;
+        }
+        
+        if (copy_to_user(buffer, etx_array, 20))
+                pr_err("Data Send : Err!\n");
+        
+        return length;;
 }
 
 /*
@@ -149,14 +137,12 @@ static ssize_t read_proc(struct file *filp, char __user *buffer, size_t length,l
 */
 static ssize_t write_proc(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
-    pr_info("proc file wrote.....\n");
-    
-    if( copy_from_user(etx_array,buff,len) )
-    {
-        pr_err("Data Write : Err!\n");
-    }
-    
-    return len;
+        pr_info("proc file wrote.....\n");
+
+        if (copy_from_user(etx_array, buff, len))
+                pr_err("Data Write : Err!\n");
+
+        return len;
 }
 
 /*
@@ -200,23 +186,19 @@ static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, 
 */
 static long etx_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-         switch(cmd) {
-                case WR_VALUE:
-                        if( copy_from_user(&value ,(int32_t*) arg, sizeof(value)) )
-                        {
-                                pr_err("Data Write : Err!\n");
-                        }
-                        pr_info("Value = %d\n", value);
-                        break;
-                case RD_VALUE:
-                        if( copy_to_user((int32_t*) arg, &value, sizeof(value)) )
-                        {
-                                pr_err("Data Read : Err!\n");
-                        }
-                        break;
-                default:
-                        pr_info("Default\n");
-                        break;
+        switch(cmd) {
+        case WR_VALUE:
+                if (copy_from_user(&value, (int32_t*)arg, sizeof(value)))
+                        pr_err("Data Write : Err!\n");
+                pr_info("Value = %d\n", value);
+                break;
+        case RD_VALUE:
+                if (copy_to_user((int32_t*) arg, &value, sizeof(value)))
+                        pr_err("Data Read : Err!\n");
+                break;
+        default:
+                pr_info("Default\n");
+                break;
         }
         return 0;
 }
@@ -227,7 +209,7 @@ static long etx_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 static int __init etx_driver_init(void)
 {
         /*Allocating Major number*/
-        if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
+        if ((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) < 0) {
                 pr_info("Cannot allocate major number\n");
                 return -1;
         }
@@ -255,12 +237,10 @@ static int __init etx_driver_init(void)
         }
         
         /*Create proc directory. It will create a directory under "/proc" */
-        parent = proc_mkdir("etx",NULL);
-        
-        if( parent == NULL )
-        {
-            pr_info("Error creating proc entry");
-            goto r_device;
+        parent = proc_mkdir("etx", NULL);
+        if (parent == NULL) {
+                pr_info("Error creating proc entry");
+                goto r_device;
         }
         
         /*Creating Proc entry under "/proc/etx/" */
